be15e3022c444a9a85e8f807007baed033f68d89
yoooousir <gimchaeyeong53@gmail.com>
2024-03-01 13:25:01 +0900

    Lotto commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lotto_Number_Generating_Simulation"
+include(":app")
+ 
